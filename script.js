5fb72dc091d08b065e122d5daf8f4fbb31a7f6b7
Min Naung <p.jean90@gmail.com>
2018-04-07 16:20:02 +0800

    upload from local

---
+++ script.js
@@ -0,0 +1,59 @@
+var latti,longi,api,weatherType,tempCacis,windSpeed,seaLevel,City,Country;
+
+$(document).ready(function(){
+	$('#info').popover({title: "<span id='siteInfo'>A <a href='https://www.freecodecamp.com/mgjean' target='_blank'>MGJEAN </a>Site.</span>",
+		content: "<span id='siteWish'>Good day amigos!</span>",html:true,placement:"left"});
+});
+
+function getLocation(){
+	var tempKelvin,image;
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			latti = position.coords.latitude;
+			longi = position.coords.longitude;
+			api = "http://api.openweathermap.org/data/2.5/weather?lat="+latti+"&lon="+longi+"&appid=5b076a73ff47357efb7ca72cf7ec0df3";			
+			$.getJSON(api,function(data){
+			
+			weatherType = data.weather[0].description;
+			$("#weat").text(weatherType);
+			
+			weatherIcon = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+			image = $(document.createElement("img"));
+			image.attr('src',weatherIcon);
+			image.css({"width":"40px","height":"40px"});
+			$("#ico").html(image);
+			
+			tempKelvin = data.main.temp;
+			tempCacis = (tempKelvin-273.15).toFixed(2)
+			$("#temp").text(tempCacis + " C");
+			
+			windSpeed  = data.wind.speed;
+			if (windSpeed >= 3 && windSpeed <= 5){
+				$("#clr-wind").addClass("success")
+			}
+			if (windSpeed < 3){
+				$("#clr-wind").addClass("info")
+			}
+			$("#windspd").text(windSpeed);
+			
+
+
+			City = data.name;
+			Country = data.sys.country;
+			$("#city").text(City +", "+Country);
+		
+			});
+
+		});
+	}
+}	
+
+function fahren(){
+	var fah = (tempCacis * 9)/5 + 32;
+	$("#temp").text( fah + " F");
+}
+
+function celsi(){
+	$("#temp").text( tempCacis + " C");
+}
+
